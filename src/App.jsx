50fa5f982dbc927a565dc49639c780b0a58a6eea
Lit-23 <rommelmarquez143@gmail.com>
2024-01-24 02:21:34 -0800

    added resume download link

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import Landing from './home/Landing';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
--- src/home/Hero.jsx
+++ src/home/Hero.jsx
@@ -38,7 +38,7 @@ function Hero() {
                         </svg>
                         Let's talk
                     </a>
-                    <a href="" className="flex text-blue-500 font-bold hover:shadow-2xl lg:mx-0 md:mx-auto duration-300 hover:shadow-blue-500/50 w-[179px] border-solid border-2 border-blue-500 rounded-lg items-center gap-3 justify-center py-2 ">
+                    <a href="https://drive.google.com/file/d/1F32xdI1ru_fHWvgxtLN5QFJqw4sJnnLm/view?usp=sharing" target='_blank' className="flex text-blue-500 font-bold hover:shadow-2xl lg:mx-0 md:mx-auto duration-300 hover:shadow-blue-500/50 w-[179px] border-solid border-2 border-blue-500 rounded-lg items-center gap-3 justify-center py-2 ">
                         <img width={24} src={download} alt='download'/>
                         Resume
                     </a>
